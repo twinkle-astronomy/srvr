060a45d0de19504eb92a7f1473a5d1b301221cad
Chris Constatnine <cconstan@gmail.com>
2026-03-27 23:06:54 -0700

    Dioxus 0.7.4

--- bootstrap.sh
+++ bootstrap.sh
@@ -7,5 +7,5 @@ mkdir .data/
 # Install cargo-binstall for fast prebuilt binary installs
 curl -L --proto '=https' --tlsv1.2 -sSf https://raw.githubusercontent.com/cargo-bins/cargo-binstall/main/install-from-binstall-release.sh | bash
 
-cargo binstall -y dioxus-cli@0.7.3
+cargo binstall -y dioxus-cli@0.7.4
 rustup component add rustfmt
